b2f801cb4e1229069568b3b3d0f1616cb2399fbf
Prasanta Ku Sethi <sethip52@gmail.com>
2023-10-12 16:25:53 +0530

    generate bill created in frontend for warden

--- NITC_Hostel_Manager_Frontend/app/src/main/java/com/kumar/nitchostelmanager/models/Bill.kt
+++ NITC_Hostel_Manager_Frontend/app/src/main/java/com/kumar/nitchostelmanager/models/Bill.kt
@@ -2,12 +2,14 @@ package com.kumar.nitchostelmanager.models
 
 data class Bill(
     var billID: String = "",
-    var studentRoll: String = "",
+    var studentRoll: String? = null,
     var amount: Double = 0.0,
     var paid: Boolean = false,
+    var billType: String = "",
     var billMonth: String = "",
     var billYear: String = "",
     var paymentID: Int = 0,
     var paymentDate: String = "",
+    var paymentTime: String = "",
     var hostelID: String = ""
 )
